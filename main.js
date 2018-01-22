1ed2c5996513ab86ce0452724658397ef6945485
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2018-01-22 10:59:41 +0100

    Fixed code conventions

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 (()=> {
-    "use strict";
-    const express = require("express");
-    const app = express();
-    const winston = require("winston");
-
-
-
-
-
-   
-    app.get("/:test/:id", (req,res) => {
-        res.end(JSON.stringify({
-            hello:req.params.id,
-            foo:req.params.test
-        }));
-    });
-    app.listen(8080, () => {
-        winston.log("info","listening");
-    });
- 
+  'use strict';
+  const express = require('express');
+  const app = express();
+  const winston = require('winston');
+  app.get('/:test/:id', (req, res) => {
+    res.end(JSON.stringify({
+      hello: req.params.id,
+      foo: req.params.test
+    }));
+  });
+  app.listen(8080, () => {
+    winston.log('info', 'listening');
+  });
 })();
